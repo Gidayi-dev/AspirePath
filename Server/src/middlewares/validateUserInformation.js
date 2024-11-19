538164303a4b89adcf391ad6a1de58ce2f36b239
Gidayi Millyannah <millyannahi@gmail.com>
2024-11-19 12:54:59 +0300

    validate user information

---
+++ Server/src/middlewares/validateUserInformation.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = newPrismaClient();
+
+async function validateUserInformation(req, res, next) {
+  const { username, email, password } = req.body;
+
+  if (!username) {
+    return res.status(400).json({ message: "Please enter your name" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ message: "Please Enter your email address" });
+  }
+  if (!password) {
+    return res.status(400).json({ message: "Please set your password" });
+  }
+
+  try {
+    const userWithEmail = await client.user.findFirst({
+      where: { email: email },
+    });
+
+    if (userWithEmail) {
+      return res.status(400).json({ message: "Email address already exists" });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error during validation:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export default validateUserInformation;
