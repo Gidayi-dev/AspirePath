e93f7456d2fece77a9fbf1a428c57987a5bb42c9
Gidayi Millyannah <millyannahi@gmail.com>
2024-11-19 13:01:22 +0300

    Validating a job

---
+++ Server/src/middlewares/validateJob.js
@@ -0,0 +1,13 @@
+function validateJob(req, res, next) {
+  const { title, location, type, description } = req.body;
+  if (!title) return res.status(400).json({ message: "Job title is required" });
+  if (!location)
+    return res.status(400).json({ message: "Job location is required" });
+  if (!type) return res.status(400).json({ message: "Job type is required" });
+  if (!description)
+    return res.status(400).json({ message: "Job description is required" });
+
+  next();
+}
+
+export default validateJob;
